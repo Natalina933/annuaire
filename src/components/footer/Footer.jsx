dff6b071aefb76f35b37dba8b69429ed97a28c14
Nathalie <nat933@free.fr>
2023-08-01 19:40:40 +0200

    image logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
 
 const Footer = () => {
     return (
         <div className={styles.container}>
-            <h3>ANOCR 15, rue Cler - 75007 Paris</h3>
-            <h3>01 44 18 92 18</h3>
-            <h4>anocr@orange.fr</h4>
+            <div>
+                <Image src="/logo.jpg" width={35} height={35} alt='contact' />
+                <h3>ANOCR 15, rue Cler - 75007 Paris</h3>
+                <h3>01 44 18 92 18</h3>
+                <h4>anocr@orange.fr</h4>
+            </div>
         </div>
-        
+
     )
 }
 
