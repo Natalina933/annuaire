74e1d22b54ca1f18f9def46a26d9cb39092edc8b
Nathalie <nat933@free.fr>
2023-08-01 17:02:10 +0200

    rendering ssr et csr

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <><div>
+            <h3>ANOCR 15, rue Cler - 75007 Paris</h3>
+            <h3>01 44 18 92 18</h3>
+            <h4>anocr@orange.fr</h4>
+        </div>
+        </>
+    )
 }
 
 export default Footer
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,36 +1,37 @@
+"use client"
 import Link from "next/link";
 import React from "react";
 
-const links=[
+const links = [
     {
-        id:1,
-        title:"Home",
-        url:"/",
+        id: 1,
+        title: "Home",
+        url: "/",
     },
     {
-        id:2,
-        title:"Conseil d'Administration",
-        url:"/conseil",
+        id: 2,
+        title: "Conseil d'Administration",
+        url: "/conseil",
     },
     {
-        id:3,
-        title:"Fiche",
-        url:"/fiche",
+        id: 3,
+        title: "Fiche",
+        url: "/fiche",
     },
     {
-        id:4,
-        title:"Groupements",
-        url:"/groupements",
+        id: 4,
+        title: "Groupements",
+        url: "/groupements",
     },
     {
-        id:5,
-        title:"Dashbord",
-        url:"/dashboard",
+        id: 5,
+        title: "Dashbord",
+        url: "/dashboard",
     },
     {
-        id:6,
-        title:"Contact",
-        url:"/contact",
+        id: 6,
+        title: "Contact",
+        url: "/contact",
     },
 ]
 
@@ -39,11 +40,15 @@ const Navbar = () => {
         <div>
             <Link href="/">ANOCR</Link>
             <div>
-                {links.map((link)=>(
+                {links.map((link) => (
                     <Link key={link.id} href={link.url}>
                         {link.title}
                     </Link>
                 ))}
+                <button
+                    onClick={() => {
+                        console.log("logged out")
+                    }}>Déconnexion</button>
             </div>
         </div>
     );
