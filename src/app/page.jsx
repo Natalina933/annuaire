4c6d0c550ecd4bfe94f539d116b0a708865c227e
Nathalie <nat933@free.fr>
2023-08-03 17:54:59 +0200

    UI Design home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,14 +4,18 @@ import Hero from 'public/hero.jpg'
 
 export default function Home() {
   return (
-<div>
-  <div className={styles.texts}>
-<h1>Annuaire des groupements de l'Association Nationale des Offiers de Carrière en retraite</h1>
-  </div>
-  <div className={styles.imgContainer}>
-    
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Annuaire des groupements de l'Association Nationale des Officiers de Carrière en retraite - ANOCR</h1>
+        <p className={styles.desc}>Membres des bureaux régionaux</p>
+        <button className={styles.button}>Voir nos réalisations</button>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.imgContainer}>
+          <Image src={Hero} className={styles.img} alt='Image contact' layout="fill" objectFit="cover" />
+        </div>
+      </div>
     </div>
-  <Image src={Hero} alt='image contact'/>
-</div>
   )
 }
