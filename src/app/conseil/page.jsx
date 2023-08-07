71e9f33d45e55cda0a98f1d5048ed515b31301ca
Nathalie <nat933@free.fr>
2023-08-07 17:36:05 +0200

    package print installé création page CA

--- src/app/conseil/page.jsx
+++ src/app/conseil/page.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import styles from "./page.module.css"
+"use client"
+import React, { useRef } from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
+import Button from '@/components/button/Button';
+import ReactToPrint from 'react-to-print';
+import Header from "public/image/arc.jpeg";
 
-const Conseil = () => {
+const Conseil = ({ grade, nom, prenom, groupement }) => {
+  const componentRef = useRef(null);
+
   return (
-    <div className={styles.container}>Liste du Conseil d'Administration</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item} ref={componentRef}>
+        <header>
+          <Image src={Header} className={styles.img} alt='Image header'  />
 
-export default Conseil
+        </header>
+        <h1 className={styles.title}>Le Bureau de l'Association</h1>
+        <ol className={styles.list}>
+          <li>Le Président</li>
+          <h3>{grade} {prenom} {nom}</h3>
+          <p>Groupement {groupement}</p>
+          <li>Les Vice-présidents</li>
+          <h3>{grade} {prenom} {nom}</h3>
+          <p>Groupement {groupement}</p>
+          <li>Le Trésorier</li>
+          <h3>{grade} {prenom} {nom}</h3>
+          <p>Groupement {groupement}</p>
+        </ol>
+      </div>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Les Membres du Conseil d'Administration</h1>
+        <ol>
+          <li>
+            <h3>{grade} {prenom} {nom}</h3>
+            <p>Groupement {groupement}</p>
+          </li>
+        </ol>
+        <div>
+          <ReactToPrint
+            trigger={() => <Button
+              url="/"
+              text="Imprimer/Télécharger"
+            />}
+            content={() => componentRef.current}
+          />
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Conseil;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from 'public/hero.jpg'
+import Button from '@/components/button/Button'
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <div className={styles.item}>
         <h1 className={styles.title}>Annuaire des groupements de l'Association Nationale des Officiers de Carrière en retraite - ANOCR</h1>
         <p className={styles.desc}>Membres des bureaux régionaux</p>
-        <button className={styles.button}>Voir nos réalisations</button>
+        <Button url="logs" text="Se connecter"/>
       </div>
 
       <div className={styles.item}>
