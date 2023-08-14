fbaba302b07c35cbdc48279fa91b746162676ad1
Nathalie <nat933@free.fr>
2023-08-14 18:35:30 +0200

    correction wrapper button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,12 +3,28 @@ import Link from "next/link";
 import React from "react";
 import styles from "./button.module.css"
 
-const Button = ({ text, url }) => {
+import { AiOutlinePrinter, AiOutlineShareAlt, AiOutlineEdit } from 'react-icons/ai'; // Importez les icônes de la bibliothèque react-icons/ai
+
+const Button = ({ url, icon }) => {
     return (
         <Link href={url}>
-            <button className={styles.container}>{text}</button>
+            <button className={styles.container}>
+                {icon}
+            </button>
         </Link>
     );
 };
 
-export default Button;
+function ButtonSection() {
+    return (
+        <section className={styles.buttonSection}>
+            <div className={styles.buttonWrapper}>
+                <Button url="/" icon={<AiOutlinePrinter />} />
+                <Button url="/" icon={<AiOutlineShareAlt />} />
+                <Button url="/" icon={<AiOutlineEdit />} />
+            </div>
+        </section>
+    );
+}
+
+export default ButtonSection;
